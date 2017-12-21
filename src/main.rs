f9e1bdfdc79bdebad2ef7c678c7730039ac9144f
natumn <natsume.pcuse75@gmail.com>
2017-12-21 17:38:24 +0900

    [Add] run_loop実装途中

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::io;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    ::std::process::exit(match run_loop() {
+       Ok(_) => 0,
+       Err(err) => {
+           writeln!(io::stderr(), "error: {:?}", err).unwrap();
+           1
+       }
+    });
+}
+
+fn run_loop() -> Result<(), ()> {
+   let args = env::args().collect();
+   let mut status = false;
+   let mut line = 
+
+   while status {
+       print!("> ");
+       line = 
+   }
+
+   Ok(())
 }
